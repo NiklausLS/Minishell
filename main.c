fa4be4e927cb2ef48b9916d8a973a1cff6b732b5
NiklausLS <nicolas.leempoels@gmail.com>
2024-06-19 18:50:16 +0200

    more changes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 02:20:08 by nileempo          #+#    #+#             */
-/*   Updated: 2024/06/19 18:11:20 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/06/19 18:16:46 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,14 @@ int main(int argc, char **argv, char **envp)
     int i = 0;
     if (argc > 1)
     {
-        while (i < argc)
+        while (argv[i])
         {
             printf("argv[%d] = %s\n", i, argv[i]);
             i++;
         }
-        //data->cmd = data->split_args[1];
-        //split_path(envp, data);
-        data->cmd = argv[1];
-        data->path = argv[2];
+        data->cmd = data->split_args[1];
         get_builtin(argv, data, envp);
-        //split_path(envp, data);
-        //check_path(data->cmd, data);
         make_path(envp, data, data->cmd);
-        //make_env(envp);
         split_redirection(argv[1], data);
         //make_redirection(argv[1]);
         print_array(data->split_args);
