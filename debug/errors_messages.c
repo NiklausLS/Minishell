488d09964b7cfe49170080ae0037946a90be0d4b
NiklausLS <nicolas.leempoels@gmail.com>
2024-07-13 19:11:42 +0200

    norminette debg errors_messages

--- debug/errors_messages.c
+++ debug/errors_messages.c
@@ -6,40 +6,35 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:23:16 by nileempo          #+#    #+#             */
-/*   Updated: 2024/07/08 22:29:00 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/07/13 19:10:09 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void    print_error(int error, char *cmd)
+void	print_error(int error, char *cmd)
 {
-    ft_putstr_fd("Minishell: ", 2);
-    if (error == 0)
-    {
-        ft_putstr_fd(cmd, 2);
-        ft_putstr_fd(": No such file or directory\n", 2);
-    }
-    else if (error == 1)
-    {
-        ft_putstr_fd(cmd, 2);
-        ft_putstr_fd(": command not found\n", 2);
-    }
-    else if (error == 2)
-        ft_putstr_fd("syntax error near unexpected token `newline'\n", 2);
-    else if (error == 3)
-    {
-        ft_putstr_fd(cmd, 2);
-        ft_putstr_fd(": permission denied\n", 2);
-    }
-    else if (error == 4)
-    {
-        ft_putstr_fd(cmd, 2);
-        ft_putstr_fd(": not a directory\n", 2);
-    }
+	ft_putstr_fd("Minishell: ", 2);
+	if (error == 0)
+	{
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+	}
+	else if (error == 1)
+	{
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd(": command not found\n", 2);
+	}
+	else if (error == 2)
+		ft_putstr_fd("syntax error near unexpected token `newline'\n", 2);
+	else if (error == 3)
+	{
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd(": permission denied\n", 2);
+	}
+	else if (error == 4)
+	{
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd(": not a directory\n", 2);
+	}
 }
-
-/*void    echo_test(t_input_data *commands)
-{
-    if (ft)
-}*/
