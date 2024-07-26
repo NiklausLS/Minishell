30ded630e2d5b525ae6db18e5fed65f1dcb6c186
NiklausLS <nicolas.leempoels@gmail.com>
2024-07-26 11:17:28 +0200

    fixing echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 19:37:56 by nileempo          #+#    #+#             */
-/*   Updated: 2024/07/25 19:34:58 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/07/26 11:16:37 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	make_echo(t_token *data)
 		printf("data->args[1] = %s\n", data->args[1]);
 	if (data->args[1])
 	{
-		i = 0;
+		i = 1;
 		while (ft_strcmp(data->args[i], "-n") == 0)
 		{
 			new_line = 0;
