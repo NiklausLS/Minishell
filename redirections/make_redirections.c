6cdd5ab3b247e6bb05f32315dc68a2a5495e05ff
NiklausLS <nicolas.leempoels@gmail.com>
2024-09-14 17:19:05 +0200

    clear in redirections

--- redirections/make_redirections.c
+++ redirections/make_redirections.c
@@ -6,7 +6,7 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 17:17:53 by nileempo          #+#    #+#             */
-/*   Updated: 2024/09/12 13:26:56 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/09/14 17:17:47 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,69 +17,44 @@ static int dup_input(int fd);
 /*
  * check if there is a input or an output and open it
  */
-/*static int	redirections(t_token *current, int *last_input, int *last_output)
-{
-	if (current->type == INPUT || current->type == HEREDOC)
-	{
-		if (*last_input != -1)
-			if (protected_close(*last_input) == 1)
-				return (1);
-		*last_input = open_input(current);
-		if (*last_input == -1)
-			return (1);
-	}
-	if (current->type == OUTPUT || current->type == APPEND)
-	{
-		if (*last_output != -1)
-			if (protected_close(*last_output) == 1)
-				return (1);
-		*last_output = open_output(current->next);
-		if (*last_output == -1)
-			return (1);
-	}
-	return (0);
-}*/
-
 static int redirections(t_token *current, int *last_input, int *last_output)
 {
-    printf("Entering redirections function\n");
     if (current->type == INPUT || current->type == HEREDOC)
     {
-        printf("Handling input redirection\n");
+        printf("IN input redirection\n");
         if (*last_input != -1)
         {
             if (protected_close(*last_input) == 1)
             {
-                printf("Error closing previous input\n");
+                ft_putstr_fd("Error: closing previous input\n", 2);
                 return (1);
             }
         }
         *last_input = open_input(current);
         if (*last_input == -1)
         {
-            printf("Error opening input file\n");
+            ft_putstr_fd("Error: opening input file\n", 2);
             return (1);
         }
     }
     if (current->type == OUTPUT || current->type == APPEND)
     {
-        printf("Handling output redirection\n");
         if (*last_output != -1)
         {
             if (protected_close(*last_output) == 1)
             {
-                printf("Error closing previous output\n");
+                ft_putstr_fd("Error: closing previous output\n", 2);
                 return (1);
             }
         }
         *last_output = open_output(current);
         if (*last_output == -1)
         {
-            printf("Error opening output file\n");
+            ft_putstr_fd("Error: opening output file\n", 2);
             return (1);
         }
     }
-    printf("Exiting redirections function\n");
+    printf("END redirections function\n");
     return (0);
 }
 
@@ -89,7 +64,7 @@ int make_all_redirections(t_token *start, t_token *end)
     int     last_input;
     int     last_output;
 
-    printf("Entering make_all_redirections\n");
+    printf("IN make_all_redirections\n");
     current = start;
     last_input = -1;
     last_output = -1;
@@ -98,7 +73,7 @@ int make_all_redirections(t_token *start, t_token *end)
         printf("Processing token: %s, type: %d\n", current->value, current->type);
         if (redirections(current, &last_input, &last_output) == 1)
         {
-            printf("Error in redirections\n");
+            ft_putstr_fd("Error: redirections\n", 2);
             return (1);
         }
         current = current->next;
@@ -106,122 +81,22 @@ int make_all_redirections(t_token *start, t_token *end)
     printf("Before dup_input\n");
     if (dup_input(last_input) == 1)
     {
-        printf("Error in dup_input\n");
+        ft_putstr_fd("Erro: in dup_input\n", 2);
         return (1);
     }
     printf("After dup_input\n");
     if (dup_output(last_output) == 1)
     {
-        printf("Error in dup_output\n");
+        ft_putstr_fd("Error: in dup_output\n", 2);
         return (1);
     }
-    printf("Exiting make_all_redirections successfully\n");
+    printf("END make_all_redirections successfully\n");
     return (0);
 }
 
-/*
- * dup the input and check if it worked
- */
-/*static int	dup_input(int fd)
-{
-	//printf("IN dup input\n");
-	//printf("fd = %d\n", fd);
-	if (fd != -1)
-	{
-		if (dup2(fd, STDIN_FILENO) == -1)
-		{
-			//ft_putstr_fd("Minishell: dup2 stdin error\n", 2);
-			close (fd);
-			return (1);
-		}
-		close (fd);
-	}
-	return (0);
-}*/
-
-/*
- * dup the output and check if it's ok
- */
-/*static int	dup_output(int fd)
-{
-	if (fd != -1)
-	{
-		if (dup2(fd, STDOUT_FILENO) == -1)
-		{
-			close(fd);
-			return (1);
-		}
-		close (fd);
-	}
-	return (0);
-}*/
-
-/*
- * Check every node and open/close input and output accordingly
- */
-/*int	make_all_redirections(t_token *start, t_token *end)
-{
-	t_token	*current;
-	int		last_input;
-	int		last_output;
-
-	current = start;
-	last_input = -1;
-	last_output = -1;
-	//printf("in make all redirections\n");
-	while (current && current != end)
-	{
-		if (redirections(current, &last_input, &last_output) == 1)
-			return (1);
-		current = current->next;
-	}
-	//printf("before dup_input\n");
-	if (dup_input(last_input) == 1)
-		return (1);
-	//printf("after dup_input\n");
-	if (dup_output(last_output) == 1)
-		return (1);
-	return (0);
-}*/
-
-/*int make_all_redirections(t_token *start, t_token *end)
-{
-    t_token *current;
-    int     last_input;
-    int     last_output;
-
-    printf("Entering make_all_redirections\n");
-    current = start;
-    last_input = -1;
-    last_output = -1;
-    while (current && current != end)
-    {
-        if (redirections(current, &last_input, &last_output) == 1)
-        {
-            printf("Error in redirections\n");
-            return (1);
-        }
-        current = current->next;
-    }
-    printf("Before dup_input\n");
-    if (dup_input(last_input) == 1)
-    {
-        printf("Error in dup_input\n");
-        return (1);
-    }
-    printf("After dup_input\n");
-    if (dup_output(last_output) == 1)
-    {
-        printf("Error in dup_output\n");
-        return (1);
-    }
-    printf("Exiting make_all_redirections successfully\n");
-    return (0);
-}*/
-
 static int dup_input(int fd)
 {
-    printf("Entering dup_input with fd=%d\n", fd);
+    printf("IN dup_input with fd=%d\n", fd);
     if (fd != -1)
     {
         if (dup2(fd, STDIN_FILENO) == -1)
@@ -238,7 +113,7 @@ static int dup_input(int fd)
 
 static int dup_output(int fd)
 {
-    printf("Entering dup_output with fd=%d\n", fd);
+    printf("IN dup_output with fd=%d\n", fd);
     if (fd != -1)
     {
         if (dup2(fd, STDOUT_FILENO) == -1)
--- redirections/opens.c
+++ redirections/opens.c
@@ -6,7 +6,7 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 17:15:59 by nileempo          #+#    #+#             */
-/*   Updated: 2024/09/12 13:26:14 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/09/14 17:14:29 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,81 +41,6 @@ int	open_input(t_token *data)
 	}
 	return (fd);
 }
-
-/*
- * open if there is an output
- */
-/*int	open_output(t_token *data)
-{
-	int		fd;
-	t_token	*current;
-
-	fd = -1;
-	current = data;
-	if (current->type == OUTPUT && current->next)
-	{
-		fd = open(current->next->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd == -1)
-		{
-			print_error(1, current->next->value);
-			current->exec_fail = 1;
-			//current->error = 1;
-		}
-	}
-	else if (current->type == APPEND && current->next)
-	{
-		fd = open(current->next->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		if (fd == -1)
-		{
-			print_error(1, current->next->value);
-			//current->exec_fail = 1;
-			current->error = 1;
-		}
-	}
-	return (fd);
-}*/
-
-/*int open_output(t_token *data)
-{
-    int     fd;
-    t_token *current;
-
-    fd = -1;
-    current = data;
-    printf("Entering open_output\n");
-    if (current->type == OUTPUT && current->next)
-    {
-        printf("Opening file %s for output\n", current->next->value);
-        fd = open(current->next->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (fd == -1)
-        {
-            perror("open failed");
-            print_error(1, current->next->value);
-            current->exec_fail = 1;
-        }
-        else
-        {
-            printf("File opened successfully with fd=%d\n", fd);
-        }
-    }
-    else if (current->type == APPEND && current->next)
-    {
-        printf("Opening file %s for append\n", current->next->value);
-        fd = open(current->next->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        if (fd == -1)
-        {
-            perror("open failed");
-            print_error(1, current->next->value);
-            current->error = 1;
-        }
-        else
-        {
-            printf("File opened successfully with fd=%d\n", fd);
-        }
-    }
-    printf("Exiting open_output with fd=%d\n", fd);
-    return (fd);
-}*/
 
 int open_output(t_token *data)
 {
